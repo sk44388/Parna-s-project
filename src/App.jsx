b807f2bb3981400513e014accdab0e5065110594
Tesla697 <96721065+Tesla697@users.noreply.github.com>
2026-09-26 00:18:50 +0530

    blah

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './App.css'
-import Mountains from './assets/mountains.png';
+import Mountains from './assets/images/mountains.png';
+import Pic1 from './assets/images/pic1.png';
+import Pic2 from './assets/images/pic2.png';
+import Pic3 from './assets/images/pic3.png';
+import Pic4 from './assets/images/pic4.png';
+
 function App() {
   return (
     <>
@@ -15,6 +20,7 @@ function App() {
   <Third/>
   <Fourth/>
   <Fifth/>
+  <Staff/>
 
     </>
   )
@@ -24,24 +30,24 @@ function First() {
   return(
     <h1>WELCOME TO THE FIRST COMPONENT</h1>
   )
-} 
+}
 function Second() {
   return(
     <h1>WELCOME TO THE SECOND COMPONENT</h1>
   )
-  
+
 }
 function Third() {
   return(
     <h1>WELCOME TO THE THIRD COMPONENT</h1>
   )
-  
+
 }
 function Fourth() {
   return(
     <h1>WELCOME TO THE FOURTH COMPONENT</h1>
   )
-  
+
 }
 function Fifth() {
   return(
@@ -50,7 +56,26 @@ function Fifth() {
     <img src={Mountains} alt={Mountains} height={'400px'} width={'400px'}></img>
     </>
   )
-  
+
+}
+
+function Staff() {
+  return(
+    <div className="staff">
+      <Card title="Vice-Principal ( Academics )" image={Pic1} />
+      <Card title="Vice-Principal ( Co-curricular )" image={Pic2} />
+      <Card title="Teacher-In Charge" image={Pic3} />
+      <Card title="Head Master" image={Pic4} />
+    </div>
+  )
+}
+
+function Card({ title, image }) {
+  return(
+    <div className="card">
+      <img src={image} alt={title} width={'250px'}></img>
+    </div>
+  )
 }
 
 export default App
